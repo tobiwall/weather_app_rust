32993587c8342a6db6e7fe1dd207fbd3436f268b
tobiwall <t.w.95@web.de>
2024-09-12 22:04:02 +0200

    use match to catch errors

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,63 @@ fn main() {
             .read_line(&mut city)
             .expect("Fehler beim Lesen der Eingabe");
 
+        city = city.trim().to_string();
+
         let api_key = env::API_KEY;
         let url = format!(
             "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric&lang=de",
             city, api_key
         );
 
-        city = city.trim().to_string();
-        let response = get(url).unwrap();
+        let response = match get(url) {
+            Ok(resp) => resp,
+            Err(e) => {
+                println!("Fehler beim Abrufen der URL: {}", e);
+                continue;
+            }
+        };
+
         let response_as_text = response.text().unwrap();
-        let json: Value = from_str(&response_as_text).expect("JSON was not well-formatted");
-        let temp = &json["main"]["temp"].as_f64().unwrap();
+        let json: Value = match from_str(&response_as_text) {
+            Ok(val) => val,
+            Err(e) => {
+                println!("Fehler beim Verarbeiten des JSONs: {}", e);
+                continue;
+            }
+        };
+
+        let temp = match json["main"]["temp"].as_f64() {
+            Some(t) => t,
+            None => {
+                println!("Konnte die Temperatur nicht abrufen.");
+                continue;
+            }
+        };
         let rounded_temp = temp.round();
-        let description = &json["weather"][0]["description"];
-        let description_str = description.as_str().unwrap_or("");
-        let description_lower_case = to_lowercase_first_char(description_str);
-        let humidity = &json["main"]["humidity"].as_u64().unwrap();
+
+        let description = match json["weather"][0]["description"].as_str() {
+            Some(desc) => desc,
+            None => {
+                println!("Konnte die Wetterbeschreibung nicht abrufen.");
+                continue;
+            }
+        };
+        let description_lower_case = to_lowercase_first_char(description);
+
+        let humidity = match json["main"]["humidity"].as_u64() {
+            Some(h) => h,
+            None => {
+                println!("Konnte die Luftfeuchtigkeit nicht abrufen.");
+                continue;
+            }
+        };
 
         println!("");
         println!(
-            "In {:?} ist es gerade {}°C, außerdem {} und die Luftfeuchtigkeit beträgt {}%",
+            "In {} ist es gerade {}°C, außerdem {} und die Luftfeuchtigkeit beträgt {}%",
             city, rounded_temp, description_lower_case, humidity
         );
-
-        println!("");
-        println!("");
+        println!("\n");
     }
 }
 
