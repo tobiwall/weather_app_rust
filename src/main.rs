d6848b66219d396c6ae0b5416c3969f852677a29
tobiwall <t.w.95@web.de>
2024-09-12 21:48:09 +0200

    loop to repead it

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,40 @@ use std::io::{self, Write};
 mod env;
 
 fn main() {
-    print!("Bitte geben Sie eine Stadt ein: ");
-    io::stdout().flush().unwrap();
-    let mut city = String::new();
-    io::stdin()
-        .read_line(&mut city)
-        .expect("Fehler beim Lesen der Eingabe");
+    loop {
+        print!("Bitte geben Sie eine Stadt ein: ");
+        io::stdout().flush().unwrap();
+        let mut city = String::new();
+        io::stdin()
+            .read_line(&mut city)
+            .expect("Fehler beim Lesen der Eingabe");
 
-    let api_key = env::API_KEY;
-    let url = format!(
-        "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric&lang=de",
-        city, api_key
-    );
+        let api_key = env::API_KEY;
+        let url = format!(
+            "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric&lang=de",
+            city, api_key
+        );
 
-    city = city.trim().to_string();
-    let response = get(url).unwrap();
-    let response_as_text = response.text().unwrap();
-    let json: Value = from_str(&response_as_text).expect("JSON was not well-formatted");
-    let temp = &json["main"]["temp"].as_f64().unwrap();
-    let rounded_temp = temp.round();
-    let description = &json["weather"][0]["description"];
-    let description_str = description.as_str().unwrap_or("");
-    let description_lower_case = to_lowercase_first_char(description_str);
-    let humidity = &json["main"]["humidity"].as_u64().unwrap();
+        city = city.trim().to_string();
+        let response = get(url).unwrap();
+        let response_as_text = response.text().unwrap();
+        let json: Value = from_str(&response_as_text).expect("JSON was not well-formatted");
+        let temp = &json["main"]["temp"].as_f64().unwrap();
+        let rounded_temp = temp.round();
+        let description = &json["weather"][0]["description"];
+        let description_str = description.as_str().unwrap_or("");
+        let description_lower_case = to_lowercase_first_char(description_str);
+        let humidity = &json["main"]["humidity"].as_u64().unwrap();
 
-    println!("");
-    println!(
-        "In {:?} ist es gerade {}°C, außerdem {} und die Luftfeuchtigkeit beträgt {}%",
-        city, rounded_temp, description_lower_case, humidity
-    );
+        println!("");
+        println!(
+            "In {:?} ist es gerade {}°C, außerdem {} und die Luftfeuchtigkeit beträgt {}%",
+            city, rounded_temp, description_lower_case, humidity
+        );
 
-    println!("");
-    println!("");
+        println!("");
+        println!("");
+    }
 }
 
 fn to_lowercase_first_char(s: &str) -> String {
@@ -50,6 +52,6 @@ fn to_lowercase_first_char(s: &str) -> String {
 
     let lower_first_char = first_char.to_lowercase();
     let result = format!("{}{}", lower_first_char, rest_of_string);
-    
+
     result
 }
